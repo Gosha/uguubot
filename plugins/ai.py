30230e65d1066c8412dc2c1b94d85ef2eafee9f1
Boltovnya <bolto@tfwno.gf>
2015-05-08 21:24:09 +0000

    Added a no-dl page

--- plugins/ai.py
+++ plugins/ai.py
@@ -27,7 +27,7 @@ responses = (
     ("wop",                     ("wop","wop")),
     ("myah",                    ("i want to die","i want to die")),
     ("i want to die",           ("MYAH!","MYAH~")),
-    ("alot",                    ("https://core.trac.wordpress.org/raw-attachment/ticket/13237/ALOT.png","ALOT~")),
+    ("alot",                    ("https://serve.empy.org/IV0zwg5xp9.png","ALOT~")),
     
 )
     # ("yeah",                    ("yeah","yeah")),
